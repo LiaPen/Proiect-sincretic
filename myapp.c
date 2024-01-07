4766f35f6903b27a0a798c97a873c0d71a3596b8
LiaPen <iulia.pentek@student.upt.ro>
2024-01-07 16:53:11 +0200

    Update myapp.c

--- myapp.c
+++ myapp.c
@@ -98,22 +98,6 @@ static int handle_request(
     return ret;
 }
 
-// Funcție de gestionare a cererilor HTTP
-int handle_request(void *cls, struct MHD_Connection *connection, const char *url,
-                   const char *method, const char *version, const char *upload_data,
-                   size_t *upload_data_size, void **con_cls) {
-    // Ignorați conținutul corpului cererii pentru acest exemplu
-    (void)upload_data;
-    (void)upload_data_size;
-
-    const char *page = "<html><body><h1>Hello, world!</h1></body></html>";
-    struct MHD_Response *response = MHD_create_response_from_buffer(strlen(page), (void *)page, MHD_RESPMEM_PERSISTENT);
-    int ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
-    MHD_destroy_response(response);
-
-    return ret;
-}
-
 int main() {
     struct MHD_Daemon *daemon;
     daemon = MHD_start_daemon(
