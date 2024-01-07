b4d019f098f94370e691aa541f389ce853690918
LiaPen <iulia.pentek@student.upt.ro>
2024-01-07 17:06:12 +0200

    Update myapp.c

--- myapp.c
+++ myapp.c
@@ -2,23 +2,51 @@
 #include <stdbool.h>
 #include <microhttpd.h>
 
-#define N 8 //Dimensiunea tablei de șah
+#define N 8 // Dimensiunea tablei de șah
 
 int tabla[N][N];
 
+// Funcție pentru rezolvarea problemei celor 8 turnuri
+bool rezolva(int col) {
+    if (col >= N) {
+        return true; // Toate turnurile au fost plasate cu succes
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (mutare_valida(i, col)) {
+            // Plasează un turn în poziția validă
+            tabla[i][col] = 1;
+
+            // Recursiv, plasează următorul turn
+            if (rezolva(col + 1)) {
+                return true;
+            }
+
+            // Dacă nu se poate plasa următorul turn, anulează mutarea
+            tabla[i][col] = 0;
+        }
+    }
+
+    return false; // Nu există o soluție validă
+}
+
+// Verifică dacă o mutare este validă
 bool mutare_valida(int row, int col) {
+    // Verifică rândul și coloana
     for (int i = 0; i < col; i++) {
         if (tabla[row][i] == 1) {
             return false;
         }
     }
 
+    // Verifică diagonala stânga-sus
     for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
         if (tabla[i][j] == 1) {
             return false;
         }
     }
 
+    // Verifică diagonala stânga-jos
     for (int i = row, j = col; i < N && j >= 0; i++, j--) {
         if (tabla[i][j] == 1) {
             return false;
@@ -28,94 +56,70 @@ bool mutare_valida(int row, int col) {
     return true;
 }
 
-bool rezolva(int col) {
-    if (col >= N) {
-        return true;
-    }
-
+// Funcție pentru afișarea tablei de șah
+void afiseaza_tabla() {
     for (int i = 0; i < N; i++) {
-        if (mutare_valida(i, col)) {
-            tabla[i][col] = 1;
-
-            if (rezolva(col + 1)) {
-                return true;
-            }
-
-            tabla[i][col] = 0;
-        }
-    }
-
-    return false;
-}
-
-void afiseaza_tabla(FILE *file) {
-    fprintf(file, "<html><body><h2>Solutia gasita:</h2><table border=\"1\">");
-
-    for (int i = 0; i < N; i++) {
-        fprintf(file, "<tr>");
         for (int j = 0; j < N; j++) {
-            fprintf(file, "<td>%d</td>", tabla[i][j]);
+            printf("%2d ", tabla[i][j]);
         }
-        fprintf(file, "</tr>");
+        printf("\n");
     }
-
-    fprintf(file, "</table></body></html>");
 }
 
-static int handle_request(
-    void *cls,
-    struct MHD_Connection *connection,
-    const char *url,
-    const char *method,
-    const char *version,
-    const char *upload_data,
-    size_t *upload_data_size,
-    void **con_cls)
-{
-    FILE *file = tmpfile();
-    if (!file) {
-        return MHD_NO;
-    }
+// Funcție de gestionare a cererilor HTTP
+int handle_request(void *cls, struct MHD_Connection *connection, const char *url,
+                   const char *method, const char *version, const char *upload_data,
+                   size_t *upload_data_size, void **con_cls) {
+    // Ignorați conținutul corpului cererii pentru acest exemplu
+    (void)upload_data;
+    (void)upload_data_size;
 
-    // Rezolvă problema celor 8 turnuri
+    // Rezolvați problema celor 8 turnuri
+    rezolva(0);
+
+    // Construiți răspunsul HTML cu tabla de șah
+    char response_content[500];
+    sprintf(response_content, "<html><body><h1>Tabla de șah:</h1><pre>");
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            sprintf(response_content + strlen(response_content), "%2d ", tabla[i][j]);
+        }
+        sprintf(response_content + strlen(response_content), "\n");
+    }
+    sprintf(response_content + strlen(response_content), "</pre></body></html>");
+
+    // Creare răspuns HTTP
+    struct MHD_Response *response = MHD_create_response_from_buffer(strlen(response_content),
+                                                                    (void *)response_content,
+                                                                    MHD_RESPMEM_PERSISTENT);
+    int ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
+    MHD_destroy_response(response);
+
+    return ret;
+}
+
+int main() {
+    // Inițializează tabla de șah cu 0
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             tabla[i][j] = 0;
         }
     }
 
-    if (rezolva(0)) {
-        afiseaza_tabla(file);
-    } else {
-        fprintf(file, "<html><body><p>Nu exista o solutie.</p></body></html>");
-    }
-
-    fseek(file, 0, SEEK_SET);
-    struct MHD_Response *response = MHD_create_response_from_fd(0, fileno(file));
-    int ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
-    MHD_destroy_response(response);
-    fclose(file);
-    return ret;
-}
-
-int main() {
-    struct MHD_Daemon *daemon;
-    daemon = MHD_start_daemon(
-        MHD_USE_SELECT_INTERNALLY,
-        8080,
-        NULL,
-        NULL,
-        &handle_request,
-        NULL,
-        MHD_OPTION_END);
+    // Configurați serverul HTTP
+    struct MHD_Daemon *daemon = MHD_start_daemon(MHD_USE_SELECT_INTERNALLY, 8080, NULL, NULL,
+                                                 &handle_request, NULL, MHD_OPTION_END);
 
     if (!daemon) {
+        fprintf(stderr, "Eroare la pornirea serverului HTTP.\n");
         return 1;
     }
 
-    printf("Serverul ruleaza pe portul 8080...\n");
-    getchar(); // Așteaptă o apăsare de tastă pentru a închide serverul
+    printf("Serverul HTTP rulează pe portul 8080. Deschideți http://localhost:8080 în browser pentru a vedea tabla de șah.\n");
+    getchar();
 
+    // Opriți serverul HTTP
     MHD_stop_daemon(daemon);
+
     return 0;
 }
